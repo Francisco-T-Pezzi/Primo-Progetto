fdd872692f130c2ad129d0a46e139ac34404dafd
Francisco-T-Pezzi <fthunderpezzi@gmail.com>
2021-05-03 20:11:45 -0300

    Atualização do projeto

--- comandi.js
+++ comandi.js
@@ -3,6 +3,19 @@ function carregar(){
     var data = new Date()
     var hora = data.getHours()
     var min  = data.getMinutes()
-    //var hora = 19
     msg.innerHTML = `Ora sono ${hora}:${(min)} ore.`
+}
+
+function carregar_dia(){
+    var msg = window.document.getElementById('msg')
+    var data = new Date()
+    var dia = data.getDate()
+    var mes = data.getMonth()
+    var ano = data.getFullYear()
+
+    msg.innerHTML = `Oggi è ${dia}/${mes}/${ano}.`
+}
+
+function mudaFoto(foto) {
+    document.getElementById("icone").src=foto;
 }
